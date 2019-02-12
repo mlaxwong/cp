9ab621bd9eed854c111afb6e42344a9616d093df
Mlax Wong <mlaxwong@gmail.com>
2019-02-12 13:40:55 +0800

    working on cp structure

--- src/Module.php
+++ src/Module.php
@@ -1,7 +1,27 @@
 <?php
 namespace paw\cp;
 
+use Yii;
+
 class Module extends \yii\base\Module
 {
-    
+    public $layout = '@pawcp_base/templates/_layouts/cp.twig';
+    protected $_viewPath = __DIR__ . '/templates';
+
+    public function getViewPath()
+    {
+        return $this->_viewPath;
+    }
+
+    public function setViewPath($viewPath)
+    {
+        $this->_viewPath = $viewPath;
+    }
+
+    public function init()
+    {
+        Yii::setAlias('@pawcp_root', dirname(__DIR__));
+        Yii::setAlias('@pawcp_base', __DIR__);
+        parent::init();
+    }
 }
